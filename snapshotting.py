0962a5aad61e4ca0345748979c6c79d442f52d7a
huguang <1349291824@qq.com>
2023-11-19 09:13:34 +0000

    snapshot

--- snapshotting.py
+++ snapshotting.py
@@ -1,15 +1,14 @@
 import redis
+import time
 
 
 r = redis.Redis(host='localhost', port=6379)
-
-for i in range(10000000):
-    r.set(str(i), str(i)*1000000)
+a = time.time()
+for i in range(8000000):
+    r.set(str(i), ''.join([str(j) for j in range(i)]))
     if not i % 100:
         print(i)
 
 r.close()
-
-
-
+print(time.time()-a)
 
